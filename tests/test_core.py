b36dd22dcb1cf55b5bf1bd64301a170ad3c94ced
Kenji Fukushima <kfuku52@gmail.com>
2026-06-26 14:01:52 +0900

    Bump version to 0.2.0

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,7 +9,7 @@ import averagers
 
 
 def test_version_is_exposed():
-    assert averagers.__version__ == "0.1.0"
+    assert averagers.__version__ == "0.2.0"
     try:
         installed_version = version("averagers")
     except PackageNotFoundError:
